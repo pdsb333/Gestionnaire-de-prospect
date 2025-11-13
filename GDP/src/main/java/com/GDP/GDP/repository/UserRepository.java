01bf27889412a9afb6da193088b5c404eb617999
fun97310 <francoise.pinceau@gmail.com>
2025-11-13 10:05:27 +0100

    Update UserRepository with annotation @Repository

--- GDP/src/main/java/com/GDP/GDP/repository/UserRepository.java
+++ GDP/src/main/java/com/GDP/GDP/repository/UserRepository.java
@@ -4,10 +4,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.GDP.GDP.entity.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }

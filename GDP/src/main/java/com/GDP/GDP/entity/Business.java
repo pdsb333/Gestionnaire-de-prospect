341d1a2418f90981fdb7ead2f1ff02aa7e2d156b
pdsb333 <francoise.pinceau@gmail.com>
2026-01-28 15:17:48 +0100

    Contrainte unique sur la propriété "name"

--- GDP/src/main/java/com/GDP/GDP/entity/Business.java
+++ GDP/src/main/java/com/GDP/GDP/entity/Business.java
@@ -3,10 +3,21 @@ package com.GDP.GDP.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "businesses")
+@Table(name = "businesses", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "name"}))
 public class Business {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

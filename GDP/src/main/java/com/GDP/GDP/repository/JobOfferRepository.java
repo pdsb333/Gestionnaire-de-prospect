0ebb9b15bc248447fa037fc284bae54bf2fc6d9a
pdsb333 <francoise.pinceau@gmail.com>
2026-02-16 20:16:21 +0100

    feat(JobOffer):  Add repository & service interface

---
+++ GDP/src/main/java/com/GDP/GDP/repository/JobOfferRepository.java
@@ -0,0 +1,8 @@
+package com.GDP.GDP.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobOfferRepository {
+    
+}
---
+++ GDP/src/main/java/com/GDP/GDP/service/joboffer/JobOfferService.java
@@ -0,0 +1,12 @@
+package com.GDP.GDP.service.joboffer;
+
+
+import com.GDP.GDP.dto.joboffer.JobOfferRequest;
+import com.GDP.GDP.dto.joboffer.JobOfferResponse;
+import com.GDP.GDP.entity.User;
+
+public interface JobOfferService {
+    JobOfferResponse create(JobOfferRequest request, Long businessId, User user);
+    JobOfferResponse updateJobOffer(JobOfferRequest request, Long id, User user);
+    void deleteJobOffer(Long id, User user);
+}

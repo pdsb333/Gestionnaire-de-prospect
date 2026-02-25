c58d2f29ef51bf15805df1b762f36af168b199b2
pdsb333 <francoise.pinceau@gmail.com>
2026-02-25 19:07:29 +0100

    fix(ProspectDTO): add some bean validations

--- GDP/src/main/java/com/GDP/GDP/dto/prospect/ProspectRequest.java
+++ GDP/src/main/java/com/GDP/GDP/dto/prospect/ProspectRequest.java
@@ -3,11 +3,13 @@ package com.GDP.GDP.dto.prospect;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 public record ProspectRequest(
-    @NotBlank LocalDateTime initialApplicationDate,
-    @NotBlank LocalDateTime dateRelaunch,
+    @NotBlank @PastOrPresent LocalDateTime initialApplicationDate,
+    @NotBlank @FutureOrPresent LocalDateTime dateRelaunch,
     @NotNull List<LocalDateTime> historyOfRelaunches
 ) {}

262428b0c7f72e0d2bd3d07d20a19c4104a768e8
pdsb333 <francoise.pinceau@gmail.com>
2026-04-02 18:38:41 +0200

    feat: add ApplicationController to handle job application creation via API endpoint

---
+++ GDP/src/main/java/com/GDP/GDP/controller/ApplicationController.java
@@ -0,0 +1,34 @@
+package com.GDP.GDP.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.GDP.GDP.dto.Application.ApplicationRequest;
+import com.GDP.GDP.dto.Application.ApplicationResponse;
+import com.GDP.GDP.security.CustomUserDetails;
+import com.GDP.GDP.service.application.ApplicationService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/application")
+public class ApplicationController {
+    private final ApplicationService applicationService;
+
+    public ApplicationController(ApplicationService applicationService){
+        this.applicationService = applicationService;
+    }
+
+    @PostMapping("/{jobOfferId}") 
+    public ResponseEntity<ApplicationResponse> createApplication(@Valid @RequestBody ApplicationRequest request, @PathVariable Long jobOfferId, @AuthenticationPrincipal CustomUserDetails user ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(applicationService.create(request, jobOfferId, user.getUser()));
+    }
+   
+}

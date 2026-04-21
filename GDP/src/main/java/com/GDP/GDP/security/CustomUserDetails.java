4a5135d1dc6d0a17b25f5a5eefb9eb475a712554
pdsb333 <francoise.pinceau@gmail.com>
2026-04-21 08:21:05 +0200

    chore: update Spring Boot version and refine security configuration

--- GDP/src/main/java/com/GDP/GDP/security/CustomUserDetails.java
+++ GDP/src/main/java/com/GDP/GDP/security/CustomUserDetails.java
@@ -24,6 +24,7 @@ public class CustomUserDetails implements UserDetails{
     @Override public String getPassword(){ return user.getPassword();}
     @Override public String getUsername(){ return user.getEmail();}
     @Override public boolean isAccountNonExpired() { return true;}
+    @Override public boolean isAccountNonLocked() { return true;}
     @Override public boolean isCredentialsNonExpired(){ return true;}
     @Override public boolean isEnabled(){ return true;}
 

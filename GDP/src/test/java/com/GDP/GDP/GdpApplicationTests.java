30f9210951d96e31d16b50a3b7bfe349d91c3791
pdsb333 <francoise.pinceau@gmail.com>
2026-04-27 21:35:56 +0200

    feat: implement AbstractIntegrationTest for integration testing with Testcontainers

--- GDP/src/test/java/com/GDP/GDP/GdpApplicationTests.java
+++ GDP/src/test/java/com/GDP/GDP/GdpApplicationTests.java
@@ -1,13 +1,15 @@
-//package com.GDP.GDP;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class GdpApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.GDP.GDP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.GDP.GDP.integration.AbstractIntegrationTest;
+
+@SpringBootTest
+class GdpApplicationTests extends AbstractIntegrationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
